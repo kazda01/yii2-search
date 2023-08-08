973b8f84054402a1d5049765992635c975647ae4
kazda01 <62554904+kazda01@users.noreply.github.com>
2023-08-08 21:23:17 +0200

    Add aria-label to search button

--- src/views/searchInput.php
+++ src/views/searchInput.php
@@ -7,7 +7,7 @@
 <form autocomplete="off" onsubmit="return false" class="<?= $formClass ?>">
     <div class="<?= $wrapperClass ?>">
         <div class="input-group">
-            <button class="search-button <?= $buttonClass ?>"><?= $buttonContent ?></button>
+            <button class="search-button <?= $buttonClass ?>" aria-label="<?= Yii::t('app', 'Search') ?>"><?= $buttonContent ?></button>
             <input id="<?= 'search-input-' . $search_id ?>" data-search-id="<?= $search_id ?>" tabindex="0" placeholder="<?= $placeholder ?>" value="" type="text" class="search-focus search-input <?= $inputClass ?>">
             <div id="<?= 'search-widget-' . $search_id ?>" class="search-widget <?= $widgetClass ?>">
             </div>
